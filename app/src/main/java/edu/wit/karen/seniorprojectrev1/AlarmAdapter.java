754cf33d1561b37d9cccc736bb08396950fac163
Karen Camaso <camasok@wit.edu>
2018-06-06 13:18:33 -0400

    First Work on Alarm Adapter

--- app/src/main/java/edu/wit/karen/seniorprojectrev1/AlarmAdapter.java
+++ app/src/main/java/edu/wit/karen/seniorprojectrev1/AlarmAdapter.java
@@ -1,10 +1,17 @@
 package edu.wit.karen.seniorprojectrev1;
 
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -12,40 +19,66 @@ import java.util.List;
  * Created by camasok on 2/16/2018.
  */
 
-public class AlarmAdapter extends  RecyclerView.Adapter<AlarmAdapter.ItemViewHolder>{
+public class AlarmAdapter extends  RecyclerView.Adapter<AlarmAdapter.ViewHolder>{
 
-    public static class ItemViewHolder extends  RecyclerView.ViewHolder
+    public List<AlarmObj> mAlarms;
+    public static class ViewHolder extends  RecyclerView.ViewHolder
     {
 
-        ItemViewHolder(View itemView){
-         super(itemView);
+        ImageView statusImage;
+        TextView alarmTime;
+        TextView alarmDate;
+        ListView list;
+        Switch activeSwitch;
+        Button moreButt;
+        Button editButt;
+        ConstraintLayout weekLayout;
+
+        CheckBox[] checkBoxes;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            statusImage = (ImageView) itemView.findViewById(R.id.timeCheck);
+            alarmTime = (TextView) itemView.findViewById(R.id.alarmTimeView);
+            alarmDate = (TextView) itemView.findViewById(R.id.alarmDateView);
+            list = (ListView) itemView.findViewById(R.id.alarm_dynamic);
+            activeSwitch = (Switch) itemView.findViewById(R.id.alarmSwitch);
+            moreButt = (Button) itemView.findViewById(R.id.alarmMoreButton);
+            editButt = (Button) itemView.findViewById(R.id.weekLayout);
+
+
         }
+
+
+
     }
 
-    List<AlarmObj> battleItemListing;
-    AlarmAdapter(List<AlarmObj> battleItemListing)
+    public AlarmAdapter(List<AlarmObj> alarum)
+
     {
-        this.battleItemListing = battleItemListing;
+        mAlarms = alarum;
     }
+
 
 
     @Override
     public int getItemCount()
     {
-        return battleItemListing.size();
+        return mAlarms.size();
     }
 
     @Override
-    public ItemViewHolder onCreateViewHolder(ViewGroup viewGroup, int i)
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i)
     {
         View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.card_layout_alarm, viewGroup, false);
-        ItemViewHolder ivh = new ItemViewHolder(v);
+        ViewHolder ivh = new ViewHolder(v);
 
         return ivh;
     }
 
     @Override
-    public void onBindViewHolder(ItemViewHolder itemViewHolder, int i)
+    public void onBindViewHolder(ViewHolder viewHolder, int i)
     {
         // Set resources here
 
